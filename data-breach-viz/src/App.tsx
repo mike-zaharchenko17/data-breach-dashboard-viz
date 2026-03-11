b719e05bcf38beb2067c4b238fdd75d101c3e3ee
mike-zaharchenko17 <mz2433@nyu.edu>
2026-03-11 09:54:02 -0400

    simple graph render in react

--- data-breach-viz/src/App.tsx
+++ data-breach-viz/src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import rawCsv from "./assets/data-breach-set-clean.csv?raw"
+import { csvParse } from 'd3-dsv'
+import { newPlot } from 'plotly.js-dist-min'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useRef, useEffect } from "react" 
+
+export default function App() {
+
+  const plotRef = useRef<HTMLDivElement | null>(null)
+
+  const methodMap : Record<string, number> = {}
+
+  const industryMap : Record<string, number> = {}
+
+  const data = csvParse(rawCsv, (d) => {
+      const formatted = {
+          entity: d.entity,
+          year: +d.year,
+          records: +d.records,
+          organization_type: d.organization_type,
+          method: d.method
+      }
+
+      methodMap[formatted.method] = methodMap[formatted.method] ? methodMap[formatted.method] += 1 : 1
+
+      industryMap[formatted.organization_type] = industryMap[formatted.organization_type] ? industryMap[formatted.organization_type] += 1 : 1
+
+      return formatted
+  })
+
+  console.log(JSON.stringify(data, null, 2))
+
+  useEffect(() => {
+    if (plotRef.current) {
+      newPlot(plotRef.current, [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16]
+      }], { margin: { t: 0 } })
+    }
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div ref={plotRef}>
+    </div>
   )
 }
 
-export default App
